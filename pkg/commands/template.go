78f965c00998d8ea60a58029b9d2eef93e7074b7
Jijesh Mohan <jijeshmohan@gmail.com>
2022-07-20 16:49:36 +0530

    Add template list feature

--- pkg/commands/template.go
+++ pkg/commands/template.go
@@ -2,9 +2,11 @@ package commands
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jijeshmohan/dago/pkg/config"
 	"github.com/jijeshmohan/dago/pkg/templates"
+	"github.com/jijeshmohan/dago/pkg/xfilesystem"
 	"github.com/urfave/cli/v2"
 )
 
@@ -45,6 +47,28 @@ func Template() *cli.Command {
 				Usage: "validate a given template",
 				Action: func(c *cli.Context) error {
 					panic("TODO")
+				},
+			},
+			{
+				Name:  "list",
+				Usage: "list all available templates",
+				Action: func(c *cli.Context) error {
+					configPath := c.String("config")
+					conf, err := config.Load(configPath)
+					if err != nil {
+						return err
+					}
+
+					repo, err := templates.NewFSRepository(xfilesystem.NewFileSystem(conf.TemplatesPath, os.DirFS(conf.TemplatesPath)))
+					if err != nil {
+						return err
+					}
+
+					for _, templateName := range repo.GetAllTemplateNames() {
+						fmt.Println(templateName)
+					}
+
+					return nil
 				},
 			},
 		},
